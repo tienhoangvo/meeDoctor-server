5b8d27aaa3a0d30dad5aed7873792a78ca06809e
Tien Hoang Vo <tienhoangvo.dev@gmail.com>
2021-03-15 21:08:36 +0700

    adding CORS

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-
-const path = require('path');
-const { static } = require('express');
+const cors = require('cors');
 const env = require('./env');
 const errorDispatcher = require('./controllers/errorDispatcher');
 const app = express();
-
 if (env.NODE_ENV === 'development') {
   // Log the http request info
   app.use(require('morgan')('dev'));
 }
+app.use(cors());
+app.options('*', cors());
 app.use(cookieParser());
 app.use(express.json({ limit: '10kb' }));
 
